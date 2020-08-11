b511ec18ab2d975b956686bcf14779152dd4fc20
xiao-moon <56753286+xiao-moon@users.noreply.github.com>
2020-08-11 22:27:08 +0800

    commit

---
+++ code-springboot2/src/main/java/mock/b_listener/A_Test.java
@@ -0,0 +1,58 @@
+package mock.b_listener;
+
+import lombok.extern.java.Log;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * 描述:测试
+ * 创建人: 小月
+ * 创建时间: 2020-08-05 23:57
+ */
+//@WebMvcTest()
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@AutoConfigureMockMvc //启动自动配置MockMVC
+@Log
+public class A_Test {
+
+    @Autowired
+    private MockMvc mockMvc; //只需 autowire
+
+    @Test
+    public void testFirst() throws Exception {
+        ResultActions actions = this.mockMvc.perform(get("/init/first").contentType(MediaType.APPLICATION_JSON));
+        MvcResult mvcResult = actions.andExpect(status().isOk()).andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        log.info("结果:"+contentAsString);
+    }
+
+    @Test
+    public void testSecond() throws Exception {
+        ResultActions actions = this.mockMvc.perform(get("/init/second").contentType(MediaType.APPLICATION_JSON));
+        MvcResult mvcResult = actions.andExpect(status().isOk()).andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        log.info("结果:"+contentAsString);
+    }
+
+    @Test
+    public void testThird() throws Exception {
+        ResultActions actions = this.mockMvc.perform(get("/init/third").contentType(MediaType.APPLICATION_JSON));
+        MvcResult mvcResult = actions.andExpect(status().isOk()).andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        log.info("结果:"+contentAsString);
+    }
+
+
+}
---
+++ code-springboot2/src/main/java/mock/b_listener/FourthListener.java
@@ -0,0 +1,20 @@
+package mock.b_listener;
+
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.SmartApplicationListener;
+import org.springframework.core.annotation.Order;
+
+@Order(4)
+public class FourthListener implements SmartApplicationListener {
+    @Override
+    public boolean supportsEventType(Class<? extends ApplicationEvent> eventType) {
+        return ApplicationStartedEvent.class.isAssignableFrom(eventType) || ApplicationPreparedEvent.class.isAssignableFrom(eventType);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        System.out.println("hello fourth");
+    }
+}
